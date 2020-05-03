c100a780b4d03d05d97739be4cc1af462c2ea3a3
alex <alex@tekartik.com>
2020-05-03 15:34:23 +0200

    fix: 2.9 formatting

--- tool/travis.dart
+++ tool/travis.dart
@@ -1,15 +1,5 @@
-import 'package:process_run/shell.dart';
+import 'package:dev_test/package.dart';
 
 Future main() async {
-  final shell = Shell();
-
-  await shell.run('''
-
-  dartanalyzer --fatal-warnings --fatal-infos .
-  dartfmt -n . --set-exit-if-changed
-
-  pub run test -p vm
-  pub run test -p chrome -j 1
-  pub run build_runner test -- -p chrome
-  ''');
+  await ioPackageRunCi('.');
 }
